b3257a0917c81ae5556faee390251c035f3c644f
kylemit <Kylemit@gmail.com>
2020-08-15 23:36:53 -0400

    style card and contents

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
 
     return {
         dir: {
